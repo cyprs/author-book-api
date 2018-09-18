52b2ae397a9d5952fa766553f4c4be62f5e5cdac
Hatice Ergün <hatice.ergun@hacettepe.edu.tr>
2018-09-18 23:44:57 +0300

    express instal and mlab db added

---
+++ helper/db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+module.exports = () => {
+    mongoose.connect('mongodb://author-book-api:123qwe@ds161112.mlab.com:61112/author-book-api', {useMongoClient: true});
+    mongoose.connection.on('open', ()=>{
+       console.log('MongoDB: Connected');
+    });
+    mongoose.connection.on('error', (err)=>{
+       console.log('MongoDB: Error', err);
+    });
+};
