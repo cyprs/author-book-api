1953fd68a3f9f74bae1285ea75e726157567897b
Hatice Ergün <hatice.ergun@hacettepe.edu.tr>
2018-09-22 00:23:57 +0300

    book list added

--- routes/book.js
+++ routes/book.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const Book = require('../models/Book');
 
-router.post('/', (req, res, next) => {
+router.post('/', (req, res) => {
     const {title, category, year, lang } = req.body;
 
     const book = new Book({
@@ -14,6 +14,15 @@ router.post('/', (req, res, next) => {
     });
 
     const promise = book.save();
+    promise.then((data) => {
+        res.json(data);
+    }).catch((err) => {
+        res.json(err);
+    });
+});
+
+router.get('/list', (req, res) => {
+    const promise = Book.find({});
     promise.then((data) => {
         res.json(data);
     }).catch((err) => {
