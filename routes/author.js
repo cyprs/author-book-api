8473c16f0a86c5358fd48903f4cff4312ed511ac
Hatice Ergün <hatice.ergun@hacettepe.edu.tr>
2018-09-22 14:22:10 +0300

    Author get and put method added

--- routes/author.js
+++ routes/author.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const router = express.Router();
+const mongoose = require('mongoose');
+const express  = require('express');
+const router   = express.Router();
 
 const Author = require('../models/Author');
 
@@ -51,7 +52,70 @@ router.get('/', (req,res) => {
 
 router.post('/', (req, res, next) => {
     const author = new Author(req.body);
-    const promise = author.save();
+    const promise = author.save(
+
+    );
+
+    promise.then((data) => {
+        res.json(data);
+    }).catch((err) => {
+        res.json(err);
+    });
+});
+
+router.put('/:author_id', (req,res) => {
+    const promise = Author.findByIdAndUpdate(req.params.author_id, req.body, {new : true});
+
+    promise.then((data) => {
+        res.json(data);
+    }).catch((err) => {
+        res.json(err);
+    });
+});
+
+router.get('/:author_id', (req,res) => {
+    const promise = Author.aggregate([
+        {
+            $match: {
+                '_id': mongoose.Types.ObjectId(req.params.author_id)
+            }
+        },
+        {
+            $lookup: {
+                from: 'books',
+                localField: '_id',
+                foreignField: 'author_id',
+                as: 'books'
+            }
+        },
+        {
+            $unwind: {
+                path: '$books',
+                preserveNullAndEmptyArrays: true
+
+            }
+        },
+        {
+            $group: {
+                _id:{
+                    _id: '$_id',
+                    name: '$name',
+                    bio: '$bio'
+                },
+                books: {
+                    $push: '$books'
+                }
+            }
+        },
+        {
+            $project: {
+                _id: '$_id.id',
+                name: '$_id.name',
+                bio: '$_id.bio',
+                books: '$books'
+            }
+        }
+    ]);
 
     promise.then((data) => {
         res.json(data);
