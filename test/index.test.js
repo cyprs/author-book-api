da798d953e3202d8ef593197d9f824fd6811d472
Hatice Ergün <hatice.ergun@hacettepe.edu.tr>
2018-09-23 21:50:33 +0300

    homepage test added

--- test/index.test.js
+++ test/index.test.js
@@ -5,8 +5,15 @@ const server = require('../app');
 
 chai.use(chaiHttp);
 
+//npm test -> komutu ile testi çalıştır.
+
 describe('Node Server', () => {
    it('(GET /) anasayfayı döndürür', (done) => {
-       done();
+       chai.request(server)
+           .get('/')
+           .end((err, res) => {
+               res.should.have.status(200);
+               done();
+           });
    });
 });
